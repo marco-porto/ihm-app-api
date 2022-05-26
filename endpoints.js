07891f3453b5930e3d5b21c16d00d615b665b86b
Marco Porto <50577988+marco-porto@users.noreply.github.com>
2022-05-26 09:18:04 +0100

    update

--- endpoints.js
+++ endpoints.js
@@ -3,9 +3,24 @@ const ads = require("./ads.json");
 const chats = require("./chats.json");
 const portfolio = require("./portfolio.json");
 
-module.exports = () => ({
-  users,
-  ads,
-  chats,
-  portfolio
-});
+const data = {
+  users: users,
+  ads:ads,
+  chats: chats,
+  portfolio: portfolio
+}
+
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router(data)
+
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+server.use(router)
+
+const port = process.env.PORT || 3000
+
+server.listen(port, () => {
+    console.log(`JSON Server is running on port ${port}`)
+})
